d42f8cfdb9e308a0f968a906daefa7fdaa2b4acf
Prem-093 <tripathiprem093@gmail.com>
2026-01-03 23:19:30 +0530

    Reverse string code

---
+++ src/main/java/JavaPractise/ReverseStringOnSamePosition.java
@@ -0,0 +1,33 @@
+package JavaPractise;
+
+public class ReverseStringOnSamePosition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="Reverse String On its Position";
+		String result="";
+		String[] strarry=str.split("\\s");
+		
+		for(String e:strarry)
+		{
+			String rev="";
+			int len=e.length();
+			
+			for(int i=len-1; i>=0; i--)
+			{
+				rev=rev+e.charAt(i);
+				
+			}
+			
+			result=result+ rev + " ";
+			
+			
+		}
+		System.out.println(result);
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/JavaPractise/StringReverse.java
@@ -0,0 +1,29 @@
+package JavaPractise;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="Prem";
+		
+		int len=str.length();
+		
+		System.out.println(len);
+		
+		System.out.println(len);
+		
+		String rev="";
+		
+		for(int i=len-1; i>=0; i--)
+		{
+			rev=rev+str.charAt(i);
+			
+		}
+		
+		System.out.println(rev);
+		
+		
+	}
+
+}
